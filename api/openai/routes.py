b5936d43f58b23454e9f42f72ae81a118ff8998e
Saint Clair <saintclair.com@gmail.com>
2023-08-01 11:47:45 -0300

    feat: layout, loading, openai ready, folders structure

--- api/openai/routes.py
+++ api/openai/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint
+import json
+import requests
 import openai
 from api.promptlayer.routes import track_request
 
@@ -13,13 +15,52 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 bp_openai = Blueprint('openai', __name__)
 
 @bp_openai.route('/completion-create', methods=['POST'])
-def completion_create(prompt, engine="text-ada-001", pl_tags=[]):
+def completion_create(prompt, pl_tags=[], engine="gpt-3.5-turbo"):
 
     # Do something fun 🚀
-    request_response = openai.Completion.create(
-        engine, 
-        prompt, 
-        pl_tags # ex:["name-guessing", "pipeline-2"]
-    )
+    url = 'https://api.openai.com/v1/chat/completions'
 
-    return track_request(request_response)
+    headers = {
+        "Authorization": f"Bearer {openai.api_key}", 
+        "Content-Type": "application/json"
+    }
+
+    body = {
+        "model": engine,
+        "messages":[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        #pl_tags # ex:["name-guessing", "pipeline-2"]
+    }
+
+    r = requests.post(url, 
+                headers=headers, 
+                data=json.dumps(body))
+
+    response = r.json()
+
+    # try:
+    #     prompt_id = ""
+    #     prompt_version = 1
+    #     prompt_input_variables = {}
+
+    #     # @TODO: check documentation promptlayer
+    #     track_request(request_response, 
+    #                 prompt, 
+    #                 prompt_id, 
+    #                 prompt_version:int, 
+    #                 prompt_input_variables, 
+    #                 function_name="openai.Completion.create",
+    #                 engine=engine, 
+    #                 tags=[])
+    # except Exception as e:
+    #     print(f'{e}')
+    
+    if 'error' in response:
+        # @TODO: tratar quando quebrar um nova requisicao talvez
+        return 'OPEN AI Error:' + response['error']['message']
+    
+    return response["choices"][0]["message"]['content']
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,86 @@
 import React, { useState } from 'react';
-import './App.css';
-import './buttons.css';
-import './pokemon-type-badge.css';
+import './assets/css/App.css';
+import './assets/css/buttons.css';
+import './assets/css/pokemon-type-badge.css';
+import openaiLogo from './assets/images/openai-logo.png';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
   const [pokemon1, setPokemon1] = useState(null);
   const [pokemon2, setPokemon2] = useState(null);
   const [battleResult, setBattleResult] = useState(null);
 
-  const gerarBatalha = async () => {
-    const response = await fetch('/gerar_batalha');
+  const generateBattle = async () => {
+    const response = await fetch('/generate_battle');
     const data = await response.json();
     setBattleResult(null);
+    let url_pokemon = 'https://pokeapi.co/api/v2';
 
-    // Obtém informações do primeiro Pokémon
-    const pokemon1Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${data.pokemon1_id}`);
+    // Get information according pokemon 1
+    const pokemon1Response = await fetch(`${url_pokemon}/pokemon/${data.pokemon1_id}`);
     const pokemon1Data = await pokemon1Response.json();
     setPokemon1(pokemon1Data);
 
-    // Obtém informações do segundo Pokémon
-    const pokemon2Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${data.pokemon2_id}`);
+    // Get information according pokemon 2
+    const pokemon2Response = await fetch(`${url_pokemon}/pokemon/${data.pokemon2_id}`);
     const pokemon2Data = await pokemon2Response.json();
     setPokemon2(pokemon2Data);
   };
 
-  const obterResultadoBatalha = async () => {
-    // Implemente a lógica para enviar os IDs dos Pokémon ao servidor Flask
-    // e obter o resultado da batalha e a explicação.
-    // O código a seguir é apenas um esboço e precisa ser implementado.
-
+  const getResultBattle = async () => {
+    setLoading(true);
+    // with pokemons get result from flask
     if (pokemon1 && pokemon2) {
-      const response = await fetch(`/resultado_batalha/${pokemon1.id}/${pokemon2.id}`);
+      const response = await fetch(`/battle_result/${pokemon1.id}/${pokemon2.id}`);
       const data = await response.json();
+      
+      setLoading(false);
       setBattleResult(data);
     }
   };
 
   return (
     <div className="container">
-      <h1 className="title">Simulador de batalhas Pokémon</h1>
+      <h1 className="title">Pokémon Battle Simulator</h1>
       <div className="button-container">
-        <button onClick={gerarBatalha} className="button generate-button">+ Nova Batalha</button>
+        <button onClick={generateBattle} className="button generate-button">+ New Battle</button>
       </div>
       <div className="pokemon-cards-container">
         {pokemon1 && (
           <div className={`pokemon-card ${battleResult && battleResult.winner === pokemon1.name ? 'winner' : ''}`}>
             <h3 className="pokemon-name">Pokémon 1: {pokemon1.name}</h3>
             <img className="pokemon-image" src={pokemon1.sprites.front_default} alt={pokemon1.name} />
-            <p className="pokemon-type">Tipo: <span className={`pokemon-type-badge ${pokemon1.types[0].type.name}`}>{pokemon1.types[0].type.name}</span></p>
+            <p className="pokemon-type">Type: <span className={`pokemon-type-badge ${pokemon1.types[0].type.name}`}>{pokemon1.types[0].type.name}</span></p>
           </div>
         )}
         {pokemon2 && (
           <div className={`pokemon-card ${battleResult && battleResult.winner === pokemon2.name ? 'winner' : ''}`}>
             <h3 className="pokemon-name">Pokémon 2: {pokemon2.name}</h3>
             <img className="pokemon-image" src={pokemon2.sprites.front_default} alt={pokemon2.name} />
-            <p className="pokemon-type">Tipo: <span className={`pokemon-type-badge ${pokemon2.types[0].type.name}`}>{pokemon2.types[0].type.name}</span></p>
+            <p className="pokemon-type">Type: <span className={`pokemon-type-badge ${pokemon2.types[0].type.name}`}>{pokemon2.types[0].type.name}</span></p>
           </div>
         )}
       </div>
       
       {pokemon1 && pokemon2 && (
         <div className="button-container">
-          <button onClick={obterResultadoBatalha}  className="button result-button">Obter Resultado</button>
+          <button onClick={getResultBattle}  className="button result-button">Get Result</button>
         </div>
+      )}
+
+      {loading ? (
+      <div className="loading">Waiting Open AI...</div>
+      ) : (
+        <div></div>
       )}
 
       {battleResult && (
         <div className="result-card">
-          <h2>Vencedor: {battleResult.winner}</h2>
-          <p>Explicação: {battleResult.explanation}</p>
+          <h2>🏆 Winner: {battleResult.winner} 
+          </h2>
+          <small>generate by: </small><img src={openaiLogo} alt="Open AI Logo" style={{ width: 100 }} />
+          <p>Explanation: {battleResult.explanation}</p>
         </div>
       )}
     </div>
